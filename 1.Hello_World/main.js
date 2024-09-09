60249d5f2e9e6f0b70983cc5d683f7474bbc4aad
sadiq khan <saiqkhan7777@gmail.com>
2024-09-09 13:34:35 +0500

    Add files via upload

---
+++ 1.Hello_World/main.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+//          variables in typescript             \\
+// let name1 = "ali"
+// console.log(name1);
+// const myName = "ali khan"
+// console.log(myName);
+// function fullName(){
+//     let name1:string = "sadiq"
+//     let name2:string = "khan"
+//     return name1 + " " + name2
+// }
+// console.log(fullName());
+var myName = ["sadiq khan", "Jasir ", "julia ann"];
+// myName.splice(1)
+myName.slice();
+console.log(myName);
